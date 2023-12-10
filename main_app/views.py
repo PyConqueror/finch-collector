8717adbfd69397b0475ba39de9714c8aa875b429
Wan Aqim <wanaqim@gmail.com>
2023-12-10 10:02:08 +0800

    added finch detail page, add url and models

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-
-finches = [
-  {'name': 'Lolo', 'species': 'Zebra Finch', 'description': 'Vibrant and active', 'age': 1},
-  {'name': 'Sachi', 'species': 'Gouldian Finch', 'description': 'Colorful and social', 'age': 2},
-  {'name': 'Milo', 'species': 'Bengalese Finch', 'description': 'Playful and curious', 'age': 3},
-  {'name': 'Bella', 'species': 'Star Finch', 'description': 'Elegant and serene', 'age': 2},
-  {'name': 'Jasper', 'species': 'Cherry Finch', 'description': 'Charming and vocal', 'age': 4}
-]
+from .models import Finch
 
 def home(request):
     return render(request, 'home.html')
@@ -15,6 +8,11 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', {
     'finches': finches
     })
+
+def finches_detail(request, finch_id):
+   finch = Finch.objects.get(id=finch_id)
+   return render(request, 'finches/detail.html', { 'finch':finch })
