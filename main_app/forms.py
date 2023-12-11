49f606c5a4da22e1c7b278dc917256224e584b12
Wan Aqim <wanaqim@gmail.com>
2023-12-11 17:11:09 +0800

    add widgets to form

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,10 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Feeding
 
-class FeedingForm(ModelForm):
-  class Meta:
-    model = Feeding
-    fields = ['date', 'meal']
+class FeedingForm(forms.ModelForm):
+    class Meta:
+      model = Feeding
+      fields = ['date', 'meal']
+      widgets = {
+          'date': forms.DateInput(attrs={'type': 'text', 'class': 'datepicker', 'id': 'id_date'}),
+      }
