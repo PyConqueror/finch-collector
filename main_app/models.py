09dca94d76543aaf25aedeff6332b3a520c23f6d
Wan Aqim <wanaqim@gmail.com>
2023-12-11 10:35:16 +0800

    update model to include return URL

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
@@ -9,3 +9,8 @@ class Finch(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+
+
