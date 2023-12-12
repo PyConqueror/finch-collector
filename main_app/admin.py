51578ae758c9db1b341eef6db2e4a518c5224310
Wan Aqim <wanaqim@gmail.com>
2023-12-12 11:02:50 +0800

    add Accessory to admin section

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Finch, Feeding
+from .models import Finch, Feeding, Accessory
 
 admin.site.register(Finch)
 admin.site.register(Feeding)
+admin.site.register(Accessory)
+
