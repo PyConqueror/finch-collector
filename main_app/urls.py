b0c6e480a69c114dd3a4ec4d08e0acb8bbbb010f
Wan Aqim <wanaqim@gmail.com>
2023-12-12 11:03:36 +0800

    add accessory URLS

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,6 +10,13 @@ urlpatterns = [
     path('finches/create/', views.FinchCreate.as_view(), name='finches_create'),
     path('finches/<int:pk>/update/', views.FinchUpdate.as_view(), name='finches_update'),
     path('finches/<int:pk>/delete/', views.FinchDelete.as_view(), name='finches_delete'),
-    path('finches/<int:finch_id>/add_feeding/', views.add_feeding, name = 'add_feeding')
+    path('finches/<int:finch_id>/add_feeding/', views.add_feeding, name = 'add_feeding'),
+    path('finches/<int:finch_id>/add_accessories/<int:accessory_id>/', views.add_accessory, name='add_accessory'),
+    path('finches/<int:finch_id>/remove_accessories/<int:accessory_id>/', views.remove_accessory, name='remove_accessory'),
+    path('accessories/', views.AccessoryList.as_view(), name='accessory_index'),
+    path('accessories/<int:pk>/', views.AccessoryDetail.as_view(), name='accessory_detail'),
+    path('accessories/create/', views.AccessoryCreate.as_view(), name='accessory_create'),
+    path('accessories/<int:pk>/update/', views.AccessoryUpdate.as_view(), name='accessory_update'),
+    path('accessories/<int:pk>/delete/', views.AccessoryDelete.as_view(), name='accessory_delete'),
 ]
 
